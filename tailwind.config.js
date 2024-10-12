2d31524f43a3c65f6fdd12b0dacf783e5f39e054
Hectorgal <145592741+Hectorgal@users.noreply.github.com>
2024-10-12 17:24:11 +0200

    Tu mensaje aquí

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,15 +10,15 @@ module.exports = {
     themes: [
       {
         lightTheme: {
-          primary: "#f4aa3a",
-          secondary: "#f4f4a1",
-          accent: "#1be885",
+          primary: "#075985",
+          secondary: "#4b5563",
+          accent: "#9d174d",
           neutral: "#272136",
           "base-100": "#ffffff",
-          info: "#778ad4",
-          success: "#23b893",
-          warning: "#f79926",
-          error: "#ea535a",
+          info: "#22d3ee",
+          success: "#34d399",
+          warning: "#ff8c00",
+          error: "#ef4444",
           body: {
             "background-color": "#e3e6e6",
           },
